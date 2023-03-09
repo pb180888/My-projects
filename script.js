d3e5c86a7e4c5e06a8150930c18545c3682f54e4
Paul <pb180888@gmail.com>
2023-03-09 12:03:16 +0200

    changed buttons Movie1 and Movie2

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const movie1 = document.querySelector("#media1");
 const movie2 = document.querySelector("#media2");
-const turnOff = document.querySelector("#media3");
+const turnOff1 = document.querySelector("#media3");
+const turnOff2 = document.querySelector("#media4");
 const openMovie1 = movie1.addEventListener("click", function () {
   document.querySelector("#video1").classList.remove("hidden");
-  turnOff.classList.remove("hidden");
+  turnOff1.classList.remove("hidden");
+  movie1.classList.add("hidden");
 });
 const openMovie2 = movie2.addEventListener("click", function () {
   document.querySelector("#video2").classList.remove("hidden");
-  turnOff.classList.remove("hidden");
+  turnOff2.classList.remove("hidden");
+  movie2.classList.add("hidden");
 });
 
-turnOff.addEventListener("click", function () {
+turnOff1.addEventListener("click", function () {
   document.querySelector("#video1").classList.add("hidden");
+  turnOff1.classList.add("hidden");
+  movie1.classList.remove("hidden");
+});
+turnOff2.addEventListener("click", function () {
   document.querySelector("#video2").classList.add("hidden");
-  turnOff.classList.add("hidden");
+  turnOff2.classList.add("hidden");
+  movie2.classList.remove("hidden");
 });
